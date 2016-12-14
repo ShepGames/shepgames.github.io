d2cb7b6b50cb9eb304b9488012a88f1458614371
FK <dextershep@gmail.com>
2016-12-14 20:28:48 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+//flappy bird game code
+//by Dexter Shepherd
+//age 15
+
 var mainState = {
     preload: function() { 
     // Load the bird sprite
-    game.load.image('bird', 'assets/bird.png'); 
+    game.load.image('bird', 'assets/kieran.png'); 
+	game.load.image('pipe', 'assets/pipe.png');
 },
 
 create: function() { 
+    this.score = 0;
+	this.labelScore = game.add.text(20, 20, "0", 
+    { font: "30px Arial", fill: "#ffffff" });  
+	this.pipes = game.add.group(); 
+	
     // Change the background color of the game to blue
     game.stage.backgroundColor = '#71c5cf';
 
@@ -25,6 +35,9 @@ create: function() {
     var spaceKey = game.input.keyboard.addKey(
                     Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);     
+	this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+	
+	
 },
 
 update: function() {
@@ -32,6 +45,8 @@ update: function() {
     // Call the 'restartGame' function
     if (this.bird.y < 0 || this.bird.y > 490)
         this.restartGame();
+	game.physics.arcade.overlap(
+    this.bird, this.pipes, this.restartGame, null, this);
 },
 // Make the bird jump 
 jump: function() {
@@ -43,6 +58,36 @@ jump: function() {
 restartGame: function() {
     // Start the 'main' state, which restarts the game
     game.state.start('main');
+},
+addRowOfPipes: function() {
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    // Add the 6 pipes 
+    // With one big hole at position 'hole' and 'hole + 1'
+    for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole + 1) 
+            this.addOnePipe(400, i * 60 + 10); 
+			this.score += 1;
+	this.labelScore.text = this.score; 
+},
+addOnePipe: function(x, y) {
+    // Create a pipe at the position x and y
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    // Add the pipe to our previously created group
+    this.pipes.add(pipe);
+
+    // Enable physics on the pipe 
+    game.physics.arcade.enable(pipe);
+
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200; 
+
+    // Automatically kill the pipe when it's no longer visible 
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
 },
 };
 
